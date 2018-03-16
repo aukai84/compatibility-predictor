e476ac597cf8ebe7e0aa95fa08a140775c546590
Aukai Tirrell <aukai.tirrell@gmail.com>
2018-03-15 19:20:15 -1000

    able to add applicants to client side.. need to import logic functions

--- app/src/App.js
+++ app/src/App.js
@@ -10,17 +10,26 @@ class App extends Component {
             team: [],
             applicants: []
         }
-        this.handleInput = this.handleInput.bind(this);
         this.calculateScore  = this.calculateScore.bind(this);
+        this.addTeamMember = this.addTeamMember.bind(this);
+        this.addApplicant = this.addApplicant.bind(this);
     }
 
-    calculateScore(){
-        console.log('calculating score...')
+    addTeamMember(person){
+        this.setState({
+            team: this.state.team.concat(person)
+        })
     }
 
-    handleInput(e){
+    addApplicant(person){
+        this.setState({
+            applicants: this.state.applicants.concat(person)
+        })
+    }
+
+    calculateScore(e){
         e.preventDefault();
-        console.log(e.target.name)
+        console.log('calculate')
     }
 
     render(){
@@ -32,13 +41,29 @@ class App extends Component {
                 <h4>Add current team members and new applicants to the app to see their compatibility score! (higher is better)</h4>
                 <div className="form-container">
                     <div className="team-form-container">
-                        <PersonForm/>
+                        <h2>New Team Member</h2>
+                        <PersonForm addPerson={this.addTeamMember}/>
                     </div>
                     <div className="applicant-form-container">
-                        <PersonForm/>
+                        <h2>New Applicant</h2>
+                        <PersonForm addPerson={this.addApplicant}/>
                     </div>
                 </div>
                 <button onClick={this.calculateScore}>Calculate Applicants Score</button>
+                <div className="person-list-container">
+                    <div className="team-members-container">
+                        <h2>Current Team</h2>
+                        {this.state.team.map(person => (
+                            <p>{person.name}</p>
+                        ))}
+                    </div>
+                    <div className="applicants-container">
+                        <h2>Current Applicants</h2>
+                    {this.state.applicants.map(person => (
+                        <p>{person.name}</p>
+                    ))}
+                    </div>
+                </div>
             </div>
         )
     }
--- app/src/components/personForm.js
+++ app/src/components/personForm.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 
-export default class MemberForm extends Component {
+export default class PersonForm extends Component {
     constructor(props){
         super(props);
         this.state = {
         }
         this.handleInput = this.handleInput.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleInput(e){
@@ -15,11 +16,16 @@ export default class MemberForm extends Component {
         })
     }
 
+    handleSubmit(e){
+        e.preventDefault();
+        this.props.addPerson(this.state)
+    }
+
     render(){
         console.log('state', this.state)
         return(
             <div className="member-form">
-                <form type="application/json" onSubmit={this.props.addTeamMember}>
+                <form type="application/json">
                     <p>Name</p>
                     <input type="text" name="name" onChange={this.handleInput}/>
                     <p>Attributes <i>*between 0-10*</i></p>
@@ -27,7 +33,7 @@ export default class MemberForm extends Component {
                     <label>python: </label><input type="number" name="python" min={0} max={10} onChange={this.handleInput}/><br></br>
                     <label>java: </label><input type="number" name="java" min={0} max={10} onChange={this.handleInput}/><br></br>
                     <label>ruby: </label><input type="number" name="ruby" min={0} max={10} onChange={this.handleInput}/><br></br>
-                    <input type="submit" value="Add Team Member"/>
+                    <input type="submit" onClick={this.handleSubmit} value="Add Team Member"/>
                 </form>
             </div>
         )
